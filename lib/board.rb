39af88de6ba175009a83405f9d60f8f4e008f1b1
Ogoidnavi <ogoidnavi13051997@gmail.com>
2023-03-02 16:51:03 +0000

    Update board class

--- lib/board.rb
+++ lib/board.rb
@@ -10,11 +10,11 @@ class Board
 
   def display_board
     puts "/n
-    #{@board[0]} | #{@board[1]} | #{@board[2]}
+    #{board[0]} | #{board[1]} | #{board[2]}
     --+---+--
-    #{@board[3]} | #{@board[4]} | #{@board[5]}
+    #{board[3]} | #{board[4]} | #{board[5]}
     --+---+--
-    #{@board[6]} | #{@board[7]} | #{@board[8]}
+    #{board[6]} | #{board[7]} | #{board[8]}
     /n"
   end
 
